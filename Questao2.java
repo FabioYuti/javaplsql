2a8b3e9d6e93ec2919eabe0cfa436d6e757f5cd5
FabioYuti <65982950+FabioYuti@users.noreply.github.com>
2021-10-26 15:23:45 -0300

    Update Questao2.java
    test GDB

--- Questao2.java
+++ Questao2.java
@@ -1,5 +1,6 @@
-public class Questao2 {
-    public static void main(String[] args){
+public class Main
+{
+	public static void main(String[] args){
         for(int i=1; i<=100; i++){
 			if ((i % 3 == 0) && (i % 5 == 0))
 				System.out.println("FooBaa");
