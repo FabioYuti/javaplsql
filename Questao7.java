64ce47639547fb12037c0a4a9ee3e92d8fae7aa6
FabioYuti <fabio@strategos.com.br>
2021-10-26 15:13:40 -0300

    test commit

---
+++ Questao7.java
@@ -0,0 +1,14 @@
+public class Questao7 {
+    public static void main(String[] args){
+		
+		String valor = "TESTE_RADEK";
+		int i=0;
+
+
+        for(char c : valor.toCharArray()){
+                i++;
+        }
+		
+		System.out.println(i);
+    }
+}
